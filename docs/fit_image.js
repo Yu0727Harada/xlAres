b019a7df213fb0a835486ae7c38af20ae24a5c0d
YuHarada <54430987+Yu0727Harada@users.noreply.github.com>
2019-12-05 17:11:11 +0900

    ヘッダーの修正。ユーザー削除の説明文を追加。 画像を調整するjavascriptを編集。

--- docs/fit_image.js
+++ docs/fit_image.js
@@ -1,19 +1,36 @@
 function image_view(){
-    let ele = document.getElementsByClassName('img_small');
-    for(let i=0;i<ele.length;i++){
-        //ele[i].style.width = 'auto';
-        var parent = ele[i].parentNode;
-        var parent_width = parent.clientWidth;
-        if(ele[i].naturalWidth >= parent_width){
-            ele[i].style.width = parent_width+'px';
+    let ele_small = document.getElementsByClassName('img_small');
+    for(let i=0;i<ele_small.length;i++){
+        //ele_small[i].style.width = 'auto';
+        var parent_small = ele_small[i].parentNode;
+        var parent_width_small = parent_small.clientWidth;
+        if(ele_small[i].naturalWidth >= parent_width_small){
+            ele_small[i].style.width = parent_width_small+'px';
         }else{
-            //ele[i].style.width = '500px';
-            ele[i].style.width = ele[i].naturalWidth+'px';
+            //ele_small[i].style.width = '500px';
+            ele_small[i].style.width = ele_small[i].naturalWidth+'px';
         }
-        ele[i].style.height = 'auto';
-        ele[i].style.padding = '20px';
-        ele[i].style.margin = 'auto';
+        ele_small[i].style.height = 'auto';
+        ele_small[i].style.padding = '20px';
+        ele_small[i].style.margin = 'auto';
     }
+
+    let ele_max = document.getElementsByClassName('img_max');
+    for(let i=0;i<ele_max.length;i++){
+        //ele_max[i].style.width = 'auto';
+        var parent_max = ele_max[i].parentNode;
+        var parent_width_max = parent_max.clientWidth;
+        if(ele_max[i].naturalWidth*0.3 >= parent_width_max){
+            ele_max[i].style.width = parent_width_max+'px';
+        }else{
+            //ele_max[i].style.width = '500px';
+            ele_max[i].style.width = ele_max[i].naturalWidth*0.3+'px';
+        }
+        ele_max[i].style.height = 'auto';
+        ele_max[i].style.padding = '20px';
+        ele_max[i].style.margin = 'auto';
+    }
+
 
 
 }
